d226c31cf369d9849d1c9a13c3d23dc2f9cfc438
VladimirB <belaevvova999@gmail.com>
2023-04-19 20:33:46 +0300

    Добавление MultiProvider

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,21 @@
+import 'package:finapp/app/models/loginpage_model.dart';
+import 'package:finapp/app/models/registerpage_model.dart';
 import 'package:flutter/material.dart';
 import 'package:finapp/app/app.dart';
 import 'package:provider/provider.dart';
 
-import 'app/model/AuthModel.dart';
 
 void main() {
   runApp(
-    ChangeNotifierProvider(
-      create: (context) => AuthModel(),
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (context) => AuthModel(),
+        ),
+        ChangeNotifierProvider(
+          create: (context) => RegisterModel(),
+        ),
+      ],
       child: const MaterialApp(
         home: App(),
       ),
