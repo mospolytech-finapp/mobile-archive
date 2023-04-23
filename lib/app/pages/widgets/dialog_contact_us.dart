ca806b0e7d7fcff83fa11dd4ef9ec5a21cbbf4bc
Evgenii Safronov <emsafronov2004@gmail.com>
2023-04-23 20:44:59 +0300

    Исправление диалогового окна

--- lib/app/pages/widgets/dialog_contact_us.dart
+++ lib/app/pages/widgets/dialog_contact_us.dart
@@ -5,63 +5,79 @@ class ContactUsDialogWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Dialog(
-      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
-      backgroundColor: Colors.grey,
-      child: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: [
-            SizedBox(height: 12),
-            Text(
-              'Связаться с нами',
-              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20, color: Color.fromRGBO(174, 216, 255, 1)),
-            ),
-            SizedBox(height: 12),
-            Text(
-              'Почта',
-              textAlign: TextAlign.start,
-              style: TextStyle(fontSize: 20, color: Color.fromRGBO(174, 216, 255, 1)),
-            ),
-            Text(
-              'HelpMe@FINAPP.COM',
-              textAlign: TextAlign.start,
-              style: TextStyle(fontSize: 20, color: Colors.white),
-            ),
-            IconButton(
-                onPressed: () {
-                  Clipboard.setData(ClipboardData(text: 'HelpMe@FINAPP.COM')).then((_) {
-                    ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text("Email скопирован")));
-                  });
-                },
-                icon: Icon(Icons.copy)),
-            SizedBox(height: 20),
-            Text(
-              'Телефон',
-              textAlign: TextAlign.start,
-              style: TextStyle(fontSize: 20, color: Color.fromRGBO(174, 216, 255, 1)),
-            ),
-            Text(
-              '89996663132',
-              textAlign: TextAlign.start,
-              style: TextStyle(fontSize: 20, color: Colors.white),
-            ),
-            IconButton(
-                onPressed: () {
-                  Clipboard.setData(ClipboardData(text: '89996663132')).then((_) {
-                    ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text("Телефон скопирован")));
-                  });
-                },
-                icon: Icon(Icons.copy)),
-            SizedBox(height: 12),
-            ElevatedButton(
-              child: Text('Close'),
-              onPressed: () => Navigator.of(context).pop(),
-            )
-          ],
-        ),
-      ),
-    );
+        child: Container(
+            decoration: const BoxDecoration(
+                borderRadius: BorderRadius.all(Radius.circular(25)),
+                gradient: LinearGradient(
+                    begin: Alignment.topLeft,
+                    end: Alignment.bottomRight,
+                    colors: [Color(0xFF3589d7), Color(0xFF02c98d)])),
+            child: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Center(
+                        child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        const SizedBox(width: 50),
+                        const Text(
+                          'Связаться с нами',
+                          textAlign: TextAlign.center,
+                          style: TextStyle(fontSize: 15, color: Colors.white),
+                        ),
+                        IconButton(
+                            onPressed: () {
+                              Navigator.pop(context);
+                            },
+                            icon: const Icon(Icons.cancel_rounded))
+                      ],
+                    )),
+                    const SizedBox(height: 12),
+                    const Text(
+                      'Почта',
+                      textAlign: TextAlign.start,
+                      style: TextStyle(fontSize: 13, color: Color(0xFF7DEDCB)),
+                    ),
+                    GestureDetector(
+                      child: const Text(
+                        'HELPME@FINAPP.COM',
+                        textAlign: TextAlign.start,
+                        style: TextStyle(fontSize: 15, color: Colors.white, decoration: TextDecoration.underline),
+                      ),
+                      onTap: () {
+                        Clipboard.setData(const ClipboardData(text: 'HELPME@FINAPP.COM')).then((_) {
+                          ScaffoldMessenger.of(context).showSnackBar(
+                              const SnackBar(content: Text("Email скопирован", style: TextStyle(color: Colors.white))));
+                        });
+                      },
+                    ),
+                    const SizedBox(height: 20),
+                    const Text(
+                      'Телефон',
+                      textAlign: TextAlign.start,
+                      style: TextStyle(fontSize: 13, color: Color(0xFF7DEDCB)),
+                    ),
+                    GestureDetector(
+                      child: const Text(
+                        '89996663132',
+                        textAlign: TextAlign.start,
+                        style: TextStyle(fontSize: 15, color: Colors.white, decoration: TextDecoration.underline),
+                      ),
+                      onTap: () {
+                        Clipboard.setData(const ClipboardData(text: '89996663132')).then((_) {
+                          ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+                              content: Text(
+                            "Телефон скопирован",
+                            style: TextStyle(color: Colors.white),
+                          )));
+                        });
+                      },
+                    ),
+                    const SizedBox(height: 30)
+                  ],
+                ))));
   }
 }
