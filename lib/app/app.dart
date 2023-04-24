fe75431f90da14a14ab0068cf598a42e7035908e
Evgenii Safronov <emsafronov2004@gmail.com>
2023-04-24 23:25:41 +0300

    Добавление локализации

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:finapp/app/components/app_navigator.dart';
 import 'package:finapp/app/components/app_themes.dart';
 import 'components/secure_storage.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 
 class App extends StatefulWidget {
   const App({super.key});
@@ -26,6 +27,14 @@ class _AppState extends State<App> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      localizationsDelegates: [
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+      ],
+      supportedLocales: const [
+        Locale('en', ''),
+        Locale('ru', ''),
+      ],
       debugShowCheckedModeBanner: false,
       title: 'Finapp',
       theme: AppTheme.apptheme(),
