18cef17731db0585969edf9eae80c6c37307705c
s4ze <imgood1104@gmail.com>
2023-05-05 21:27:43 +0300

    Вёрстка диалоговых окон на экране транзакций

--- lib/app/pages/homepage/pages/transactions/transactionspage_page.dart
+++ lib/app/pages/homepage/pages/transactions/transactionspage_page.dart
@@ -1,5 +1,8 @@
+import 'dart:ui';
+import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:finapp/app/pages/homepage/pages/transactions/widgets/transactionspage_dialog.dart';
 
 class TransactionsPage extends StatefulWidget {
   const TransactionsPage({super.key});
@@ -11,6 +14,44 @@ class TransactionsPage extends StatefulWidget {
 class _TransactionsPageState extends State<TransactionsPage> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Placeholder(
+      child: Scaffold(
+        body: Center(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              OutlinedButton(
+                onPressed: () {
+                  showDialog(
+                    context: context,
+                    builder: (context) => const DeleteTransactionDialogWidget(),
+                  );
+                },
+                child: const Text("Удалить транзакцию"),
+              ),
+              OutlinedButton(
+                onPressed: () {
+                  showDialog(
+                    context: context,
+                    builder: (context) => const EditTransactionDialogWidget(),
+                  );
+                },
+                child: const Text("Изменить транзакцию"),
+              ),
+              OutlinedButton(
+                onPressed: () {
+                  showDialog(
+                    context: context,
+                    builder: (context) => const AddTransactionDialogWidget(),
+                  );
+                },
+                child: const Text("Добавить транзакцию"),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
