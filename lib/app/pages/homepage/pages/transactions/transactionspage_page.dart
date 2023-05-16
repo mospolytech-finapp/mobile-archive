ff67a16239df6dfcb1a2e1971b89038c9d195c1a
Evgenii Safronov <emsafronov2004@gmail.com>
2023-05-16 19:10:53 +0300

    Добавление slidable

--- lib/app/pages/homepage/pages/transactions/transactionspage_page.dart
+++ lib/app/pages/homepage/pages/transactions/transactionspage_page.dart
@@ -6,6 +6,7 @@ import 'package:flutter_svg/flutter_svg.dart';
 import 'package:provider/provider.dart';
 import 'package:sizer/sizer.dart';
 import 'package:finapp/app/pages/homepage/pages/transactions/widgets/listview_item_widget.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
 
 List<String> list = <String>['Категория', 'Дата', 'Цели', 'Сумма', 'Время'];
 
@@ -23,8 +24,6 @@ class _TransactionsPageState extends State<TransactionsPage> {
   int count = 0;
   int i = 0;
 
-
-
   @override
   Widget build(BuildContext context) {
     return MultiProvider(
@@ -35,8 +34,7 @@ class _TransactionsPageState extends State<TransactionsPage> {
       ],
       child: Builder(
         builder: (context) {
-
-          if( i == 0){
+          if (i == 0) {
             Provider.of<Transactions_model>(context, listen: false).loadTransactions();
             Provider.of<Transactions_model>(context, listen: false).loadCategories();
             i++;
@@ -47,8 +45,9 @@ class _TransactionsPageState extends State<TransactionsPage> {
           Map<DateTime, List<Transaction>> groupTransactionsByDate(List<Transaction> transactions) {
             return groupBy(transactions, (Transaction t) => DateTime(t.date.year, t.date.month, t.date.day));
           }
-          Map<DateTime, List<Transaction>> groupedTransactions = groupTransactionsByDate(transactionsModel.transactions ?? []);
 
+          Map<DateTime, List<Transaction>> groupedTransactions =
+              groupTransactionsByDate(transactionsModel.transactions ?? []);
 
           return Stack(
             fit: StackFit.expand,
@@ -71,8 +70,7 @@ class _TransactionsPageState extends State<TransactionsPage> {
                               style: TextButton.styleFrom(
                                   shadowColor: Color(0xff383737),
                                   backgroundColor: const Color(0xff00B881),
-                                  shape: RoundedRectangleBorder(
-                                      borderRadius: BorderRadius.circular(35))),
+                                  shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(35))),
                               child: DropdownButton(
                                 hint: Text(
                                   'Фильтры',
@@ -97,8 +95,7 @@ class _TransactionsPageState extends State<TransactionsPage> {
                                   setState(() {});
                                 },
                                 items: list.map((itemone) {
-                                  return DropdownMenuItem(
-                                      value: itemone, child: Text(itemone));
+                                  return DropdownMenuItem(value: itemone, child: Text(itemone));
                                 }).toList(),
                               ),
                             ),
@@ -115,19 +112,17 @@ class _TransactionsPageState extends State<TransactionsPage> {
                                 print(transactionsModel.categories);
                                 print(transactionsModel.transactions);
                                 transactionsModel.addTransaction(
-                                  category: transactionsModel.categories![1].id,
-                                  title: 'fgfg',
-                                  date: DateTime.now(),
-                                  amount: -9000.0,
-                                  time:'18:00' ,
-                                  description: 'hsdkjhfshdfhkjshfhskjhfhshfkjsh');
-
-                                },
+                                    category: transactionsModel.categories![1].id,
+                                    title: 'fgfg',
+                                    date: DateTime.now(),
+                                    amount: -9000.0,
+                                    time: '18:00',
+                                    description: 'hsdkjhfshdfhkjshfhskjhfhshfkjsh');
+                              },
                               style: TextButton.styleFrom(
                                   backgroundColor: const Color(0xff00B881),
                                   elevation: 0,
-                                  shape: RoundedRectangleBorder(
-                                      borderRadius: BorderRadius.circular(35))),
+                                  shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(35))),
                               child: Text(
                                 'Добавить',
                                 style: TextStyle(
@@ -151,8 +146,7 @@ class _TransactionsPageState extends State<TransactionsPage> {
                               style: TextButton.styleFrom(
                                   backgroundColor: const Color(0xff00B881),
                                   elevation: 0,
-                                  shape: RoundedRectangleBorder(
-                                      borderRadius: BorderRadius.circular(35))),
+                                  shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(35))),
                               child: Text(
                                 'Редактировать',
                                 style: TextStyle(
@@ -176,31 +170,65 @@ class _TransactionsPageState extends State<TransactionsPage> {
                         final date = groupedTransactions.keys.elementAt(index);
                         final transactions = groupedTransactions[date];
                         return Padding(
-                          padding: EdgeInsets.only(bottom: 2.5.h),
-                          child: GestureDetector(
-                            onTap: () {
-                              setState(() {
-                                if (selectedIndex == -1) {
-                                  selectedIndex = index;
-                                } else if (selectedIndex == index) {
-                                  selectedIndex = -1;
-                                } else if (selectedIndex != index) {
-                                  selectedIndex = index;
-                                }
-                              });
-                            },
-                            child: AnimatedContainer(
-                              height: calculate_height(index, selectedIndex),
-                              curve: Curves.easeInOutCubic,
-                              duration: const Duration(milliseconds: 1000),
-                              child: ListViewItemWidget(
-                              index: index,
-                              transactions: transactions ?? [],
-                              // calculate_height: calculate_height(index, selectedIndex),
-                            ),
-                          ),
-                          )
-                        );
+                            padding: EdgeInsets.only(bottom: 2.5.h),
+                            child: GestureDetector(
+                              onTap: () {
+                                setState(() {
+                                  if (selectedIndex == -1) {
+                                    selectedIndex = index;
+                                  } else if (selectedIndex == index) {
+                                    selectedIndex = -1;
+                                  } else if (selectedIndex != index) {
+                                    selectedIndex = index;
+                                  }
+                                });
+                              },
+                              child: Slidable(
+                                key: const ValueKey(0),
+                                startActionPane: ActionPane(
+                                  motion: ScrollMotion(),
+                                  children: [
+                                    Expanded(
+                                      child: Container(
+                                          decoration: BoxDecoration(
+                                              color: Color(0xff383737),
+                                              borderRadius: BorderRadius.all(Radius.circular(20))),
+                                          child: Center(
+                                            child: Row(
+                                              mainAxisAlignment: MainAxisAlignment.center,
+                                              children: [
+                                                IconButton(
+                                                    onPressed: () {},
+                                                    icon: Icon(
+                                                      Icons.tune,
+                                                      color: Colors.white,
+                                                      size: 10.w,
+                                                    )),
+                                                IconButton(
+                                                    onPressed: () {},
+                                                    icon: Icon(
+                                                      Icons.delete,
+                                                      color: Colors.white,
+                                                      size: 10.w,
+                                                    ))
+                                              ],
+                                            ),
+                                          )),
+                                    ),
+                                  ],
+                                ),
+                                child: AnimatedContainer(
+                                  height: calculate_height(index, selectedIndex),
+                                  curve: Curves.easeInOutCubic,
+                                  duration: const Duration(milliseconds: 1000),
+                                  child: ListViewItemWidget(
+                                    index: index,
+                                    transactions: transactions ?? [],
+                                    // calculate_height: calculate_height(index, selectedIndex),
+                                  ),
+                                ),
+                              ),
+                            ));
                       },
                     ),
                   ),
