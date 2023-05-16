682618a14d34473354b05ac2a111bae46486a1a7
pleaqueet <pleaqueet02@gmail.com>
2023-05-16 17:10:59 +0300

    ColorScheme & замена CircularProgressIndicator

--- lib/app/components/app_themes.dart
+++ lib/app/components/app_themes.dart
@@ -2,6 +2,9 @@ import 'package:flutter/material.dart';
 
 abstract class AppTheme {
   static ThemeData apptheme() {
-    return ThemeData(useMaterial3: true);
+    return ThemeData(
+        useMaterial3: true,
+        colorScheme: ColorScheme.fromSeed(
+            brightness: Brightness.light, seedColor: const Color(0xFF1BD0B8)));
   }
 }
